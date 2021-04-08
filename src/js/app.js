f92a45cde440006943effa148e40fe85e1f8f608
vohongnga <ngachedunhubo99>
2021-04-08 15:51:48 +0700

    UI Shiningtea used css & bootstrap4

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,20 @@ require('../scss/style.scss');
 
 console.log('Paste your code here!');
 console.log(testModules.hello);
+jQuery(document).ready(function($){ 	
+	if($(".btn-top").length > 0){
+		$(window).scroll(function () {
+			var e = $(window).scrollTop();
+			if (e > 300) {
+				$(".btn-top").show()
+			} else {
+				$(".btn-top").hide()
+			}
+		});
+		$(".btn-top").click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			})
+		})
+	}		
+});
